a9594408354b39f1ddf75b1735e789f76c525caf
dengming881223 <dengming881223@gmail.com>
2024-04-22 16:01:06 +0800

    devcontainer配置

---
+++ .devcontainer/bin/start.sh
@@ -0,0 +1,5 @@
+sudo cp /workspace/.devcontainer/conf/my.list /etc/apt/sources.list.d/
+# export https_proxy=http://192.168.1.101:23457
+# export http_proxy=http://192.168.1.101:23457
+echo 'hello' > ~/3.txt
+sleep infinity
